a8b2ca1aa88995d9a68ef2c5d6cda4e5b4400c68
Hunam <38606542+Hunam6@users.noreply.github.com>
2021-05-01 15:10:11 +0100

    feat(all): Better menu (translated and sorted)

--- src/all.ts
+++ src/all.ts
@@ -5,6 +5,8 @@ export const all = async (doc: Document, lang: string) => {
   let data: {
     lang: string
     query: string
+    shownMenu: Record<string, string>[]
+    hiddenMenu: Record<string, string>[]
     proposition: Record<string, Record<string, string>>
     hasProposition: boolean
     firstResult: Record<string, string>
@@ -17,10 +19,11 @@ export const all = async (doc: Document, lang: string) => {
       desc: string
       infos: Record<string, string>[]
     }
-    relatedSearchs: string[]
   } = {
     lang: lang,
     query: doc.querySelector('title')!.textContent.split(' - ')[0],
+    shownMenu: [],
+    hiddenMenu: [],
     proposition: {
       proposition: {
         text: '',
@@ -47,10 +50,29 @@ export const all = async (doc: Document, lang: string) => {
       subtitle: '',
       desc: '',
       infos: []
-    },
-    relatedSearchs: []
+    }
   }
 
+  //Get menu infos
+  let str = ''
+  doc.querySelectorAll('script').forEach(el => el.textContent.includes('(function(){var u=') ? str = el.textContent.split('var m=[')[1].split(';')[0] : null)
+  const menuIDs = ['WEB', 'IMAGES', 'VIDEOS', 'NEWS', 'SHOPPING', 'BOOKS', 'MAPS', 'FLIGHTS', 'FINANCE']
+  const baseMenu: any[] = []
+  menuIDs.forEach((_, i) => baseMenu.push([str.indexOf(menuIDs[i]), str.split(menuIDs[i])[0].split('\\x22')[str.split(menuIDs[i])[0].split('\\x22').length - 3].split('\\x22')[0]]))
+  baseMenu.sort((a, b) => a[0] > b[0] ? 1 : -1)
+  baseMenu.forEach((el, i) =>
+    i < 5 ?
+      data.shownMenu.push({
+        id: menuIDs[i] !== 'WEB' ? menuIDs[i].toLowerCase() : 'all',
+        value: el[1]
+      })
+      : data.hiddenMenu.push({
+        id: menuIDs[i],
+        value: el[1]
+      }))
+  //TODO: Implement "hiddenMenu" aka "more" on google.com to access others menus
+
+  //Results
   doc.querySelectorAll('.LC20lb.DKV0Md').forEach((el, i) => (data.results[i] = {title: el.textContent})) //results title
   doc.querySelectorAll('.aCOpRe').forEach((el, i) => (data.results[i].desc = el.textContent)) //results description
   doc.querySelectorAll('.TbwUpd.NJjxre').forEach((el, i) => (data.results[i].shownLink = el.textContent)) //results shown link
@@ -61,6 +83,7 @@ export const all = async (doc: Document, lang: string) => {
   doc.querySelectorAll('.l').forEach((el, i) => (data.firstResults[i].link = el.parentElement!.children[0].getAttribute('href')!)) //first results link
   data.firstResults = data.firstResults.slice(0, 4) //Better visibility
 
+  //Knowledge panel
   if (doc.querySelector('.wwUB2c') != null) {
     data.knwlPanel.title = doc.querySelector('.qrShPb')!.textContent //knowledge panel title
     data.knwlPanel.subtitle = doc.querySelector('.wwUB2c')!.textContent //knowledge panel subtitle
@@ -70,6 +93,7 @@ export const all = async (doc: Document, lang: string) => {
     //TODO: remove unwanted elements from knowledge panel infos (eg: q=toyota)
   } else data.hasKnwlPanel = false
 
+  //Spelling check
   if (doc.querySelector('.spell_orig') != null) {
     data.proposition.proposition.text = doc.querySelector('.gL9Hy')!.textContent
     data.proposition.proposition.data = doc.querySelectorAll('.gL9Hy')![1].textContent
